8f32b54f4f6438e49b25d8d1b9545eec35ac9143
Daniel9822 <dhdany64@gmail.com>
2022-12-14 23:37:23 -0400

    Access-control

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ server.use(cookieParser());
 server.use(express.json())
 server.use(morgan('dev'));
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://proyect-food-backend-production.up.railway.app/'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Origin', 'https://food-proyect.onrender.com/'); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
